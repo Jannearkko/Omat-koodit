a5c48f1fd3ded9a554cf2cc9d9835bc96042c1fd
Janne Arkko <ab3817@student.jamk.fi>
2023-10-31 09:26:30 +0200

    ha7 poistettu

--- ha7/lib/game.exs
+++
@@ -1,8 +0,0 @@
-defmodule Game do
-  import BlackJackGame
-  import GameCoordinator
-  import Player
-  import Dealer
-
-  GameCoordinator.start_game()
-end
--- ha7/test/ha7_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HA7Test do
-  use ExUnit.Case
-  doctest HA7
-
-  test "greets the world" do
-    assert HA7.hello() == :world
-  end
-end
