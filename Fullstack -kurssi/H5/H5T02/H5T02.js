8c0f14e698138e6c7c4f5c4af4e2e75bd9e1dfc5
Janne Arkko <ab3817@student.jamk.fi>
2024-03-10 10:10:32 +0200

    Siistitty repoa

--- Fullstack -kurssi/H5/H5T02/H5T02.js
+++
@@ -1,8 +0,0 @@
-// User moduuli käyttöön
-const ukkeli = require("./user")
-
-let name = "Turo Kiiski"
-let location = "Vittulanjänkä"
-let bd = ukkeli.bd
-
-console.log(ukkeli.getName(name) + ukkeli.getLocation(location)+ bd)
